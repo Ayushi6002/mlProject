2996e3907a61befdf129777393c835bdddd02cd2
Ayushi Jain <ayushi1219jain@gmail.com>
2025-07-22 03:55:46 +0530

    Data Ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,9 +1,9 @@
 import sys
-import logging
+import logging  # use Python's logging or import from your custom logger if available
 
 # Exception Handler Function
 def error_message_detail(error, error_detail: sys):
-    _, _, exc_tb = error_detail.exc_info()
+    exc_type, exc_value, exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
     error_message = "Error occurred in Python script name [{0}] line number [{1}] error message [{2}]".format(
         file_name, exc_tb.tb_lineno, str(error))
@@ -12,20 +12,10 @@ def error_message_detail(error, error_detail: sys):
 # Custom Exception Class
 class CustomException(Exception):
     def __init__(self, error, error_detail: sys):
+        super().__init__(error_message_detail(error, error_detail))
         self.error_message = error_message_detail(error, error_detail)
-        super().__init__(self.error_message)
+
 
     def __str__(self):
         return self.error_message
 
-# Main Test Block
-if __name__ == "__main__":
-    logging.basicConfig(filename="error.log", level=logging.INFO,
-                        format="%(asctime)s %(levelname)s %(message)s")
-    try:
-        a = 1 / 0
-    except Exception as e:
-        logging.info("Divide by zero exception occurred")
-        raise CustomException(e, sys)
-
-        
